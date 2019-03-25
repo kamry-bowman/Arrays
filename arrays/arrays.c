75b992038fda95d7fd99cb554c143d8a5edad250
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-25 16:19:42 -0600

    append and insert working w/ linkedl ist

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -3,11 +3,17 @@
 #include <string.h>
 #include <errno.h>
 
+typedef struct Node
+{
+  struct Node *next;
+  char *value;
+} Node;
+
 typedef struct Array
 {
-  int capacity;    // How many elements can this array hold?
-  int count;       // How many states does the array currently hold?
-  char **elements; // The string elements contained in the array
+  Node *head;
+  Node *tail;
+  int count; // How many states does the array currently hold?
 } Array;
 
 /************************************
@@ -19,19 +25,24 @@ typedef struct Array
 /*****
  * Allocate memory for a new array
  *****/
-Array *create_array(int capacity)
+Array *
+create_array()
 {
   Array *array = malloc(sizeof(Array));
-  // Allocate memory for the Array struct
-  // Set initial values for capacity and count
   array->count = 0;
-  array->capacity = capacity;
 
-  // Allocate memory for elements
-  char **elements = malloc(capacity * sizeof(char **));
-  array->elements = elements;
+  array->head = NULL;
+  array->tail = NULL;
 
   return array;
+}
+
+Node *create_node(Node *next, char *value)
+{
+  Node *node = malloc(sizeof(Node));
+  node->next = next;
+  node->value = strdup(value);
+  return node;
 }
 
 /*****
@@ -39,38 +50,17 @@ Array *create_array(int capacity)
  *****/
 void destroy_array(Array *arr)
 {
-
   // Free all elements
-  for (int i = 0; i < arr->capacity; i++)
+  Node *current = arr->head;
+  while (current)
   {
-    free(arr->elements[i]);
+    Node *next = current->next;
+    free(current);
+    current = next;
   }
 
   // Free array
   free(arr);
-}
-
-/*****
- * Create a new elements array with double capacity and copy elements
- * from old to new
- *****/
-void resize_array(Array *arr)
-{
-
-  // Create a new element storage with double capacity
-  char **new_elements = malloc(sizeof(char **) * arr->capacity * 2);
-
-  // Copy elements into the new storage
-  for (int i = 0; i < arr->count; i++)
-  {
-    new_elements[i] = arr->elements[i];
-  }
-
-  // Free the old elements array (but NOT the strings they point to)
-  free(arr->elements);
-  // Update the elements and capacity to new values
-  arr->elements = new_elements;
-  arr->capacity = arr->capacity * 2;
 }
 
 /************************************
@@ -108,41 +98,15 @@ char *arr_read(Array *arr, int index)
     perror("Index out of range");
     return NULL;
   }
-  return arr->elements[index];
-}
-
-/*****
- * Insert an element to the array at the given index
- *****/
-void arr_insert(Array *arr, char *element, int index)
-{
-
-  // Throw an error if the index is greater than the current count
-  index = process_index(arr, index);
-  if (index == -1)
+  int current_index = 0;
+  Node *current = arr->head;
+  while (current_index != index)
   {
-    perror("Index out of range");
-    exit(1);
-  }
-  int old_count = arr->count;
-
-  // Resize the array if the number of elements is over capacity
-  if (arr->count == arr->capacity)
-  {
-    resize_array(arr);
+    current = current->next;
+    current_index++;
   }
 
-  // Move every element after the insert index to the right one position
-  for (int i = old_count; i > index; i--)
-  {
-    arr->elements[i] = arr->elements[i - 1];
-  }
-
-  // Copy the element and add it to the array
-  arr->elements[index] = strdup(element);
-
-  // Increment count by 1
-  arr->count++;
+  return current->value;
 }
 
 /*****
@@ -150,15 +114,60 @@ void arr_insert(Array *arr, char *element, int index)
  *****/
 void arr_append(Array *arr, char *element)
 {
-  // Resize the array if the number of elements is over capacity
-  if (arr->count == arr->capacity)
+  if (arr->count == 0)
   {
-    resize_array(arr);
+    arr->head = create_node(NULL, element);
+    arr->tail = arr->head;
   }
-  // Copy the element and add it to the end of the array
-  arr->elements[arr->count] = strdup(element);
-  // Increment count by 1
+  else
+  {
+    arr->tail->next = create_node(NULL, element);
+    arr->tail = arr->tail->next;
+  }
   arr->count++;
+}
+
+/*****
+ * Insert an element to the array at the given index
+ *****/
+void arr_insert(Array *arr, char *element, int index)
+{
+  // handle adding to tail
+  if (index == arr->count)
+  {
+    arr_append(arr, element);
+  }
+  else
+  {
+
+    // Throw an error if the index is greater than the current count
+    index = process_index(arr, index);
+
+    if (index == -1)
+    {
+      perror("Index out of range");
+      exit(1);
+    }
+
+    if (index == 0)
+    {
+      Node *old_head = arr->head;
+      arr->head = create_node(old_head, element);
+    }
+    else
+    {
+      int current_index = 0;
+      Node *current = arr->head;
+      while (current_index != index - 1)
+      {
+        current = current->next;
+        current_index++;
+      }
+      current->next = create_node(current->next, element);
+    }
+    // Increment count by 1
+    arr->count++;
+  }
 }
 
 /*****
@@ -167,36 +176,36 @@ void arr_append(Array *arr, char *element)
  *
  * Throw an error if the value is not found.
  *****/
-void arr_remove(Array *arr, char *element)
-{
+// void arr_remove(Array *arr, char *element)
+// {
 
-  // Search for the first occurence of the element and remove it.
-  // Don't forget to free its memory!
-  int found = -1;
-  for (int i = 0; i < arr->count; i++)
-  {
-    int string_match = strcmp(element, arr->elements[i]);
-    if (string_match == 0)
-    {
-      found = i;
-      break;
-    }
-  }
+//   // Search for the first occurence of the element and remove it.
+//   // Don't forget to free its memory!
+//   int found = -1;
+//   for (int i = 0; i < arr->count; i++)
+//   {
+//     int string_match = strcmp(element, arr->elements[i]);
+//     if (string_match == 0)
+//     {
+//       found = i;
+//       break;
+//     }
+//   }
 
-  if (found == -1)
-  {
-    perror("Item not found");
-    exit(1);
-  }
-  // Shift over every element after the removed element to the left one position
-  for (int j = found; j < arr->count - 1; j++)
-  {
-    arr->elements[j] = arr->elements[j + 1];
-  }
+//   if (found == -1)
+//   {
+//     perror("Item not found");
+//     exit(1);
+//   }
+//   // Shift over every element after the removed element to the left one position
+//   for (int j = found; j < arr->count - 1; j++)
+//   {
+//     arr->elements[j] = arr->elements[j + 1];
+//   }
 
-  // Decrement count by 1
-  arr->count--;
-}
+//   // Decrement count by 1
+//   arr->count--;
+// }
 
 /*****
  * Utility function to print an array.
@@ -204,9 +213,11 @@ void arr_remove(Array *arr, char *element)
 void arr_print(Array *arr)
 {
   printf("[");
+  Node *current = arr->head;
   for (int i = 0; i < arr->count; i++)
   {
-    printf("%s", arr->elements[i]);
+    printf("%s", current->value);
+    current = current->next;
     if (i != arr->count - 1)
     {
       printf(",");
@@ -219,7 +230,7 @@ void arr_print(Array *arr)
 int main(void)
 {
 
-  Array *arr = create_array(1);
+  Array *arr = create_array();
   // arr_append(arr, "test0");
   // arr_append(arr, "test1");
   // arr_append(arr, "test2");
@@ -250,8 +261,8 @@ int main(void)
   arr_insert(arr, "STRING2", 0);
   arr_insert(arr, "STRING3", 1);
   arr_print(arr);
-  arr_remove(arr, "STRING3");
-  arr_print(arr);
+  // arr_remove(arr, "STRING3");
+  // arr_print(arr);
 
   destroy_array(arr);
 
